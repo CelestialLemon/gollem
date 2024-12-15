b01687cb72a27fe6a385d885b21bf3623070ad25
Ashutosh Shinde <ashutoshinde23@gmail.com>
2024-12-15 13:51:20 +0530

    first basic implementation of llm requests

---
+++ src/config.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/BurntSushi/toml"
+)
+
+type Config struct {
+	ApiKey     string
+	ApiBaseUrl string
+}
+
+func LoadConfig(path string) (*Config, error) {
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		return nil, fmt.Errorf("config file not found: %s", path)
+	}
+
+	var config Config
+
+	if _, err := toml.DecodeFile(path, &config); err != nil {
+		return nil, err
+	}
+
+	return &config, nil
+}
--- src/main.go
+++ src/main.go
@@ -1,7 +1,90 @@
 package main
 
-import "fmt"
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+)
+
+type Message struct {
+	Role    string `json:"role"`
+	Content string `json:"content"`
+}
+
+type ApiRequestBody struct {
+	Model    string    `json:"model"`
+	Messages []Message `json:"messages"`
+}
+
+type ApiResponseBody struct {
+	Choices []struct {
+		Message Message `json:"message"`
+	} `json:"choices"`
+}
+
+func llm(config Config) string {
+	// create request body
+	requestBody := ApiRequestBody{
+		Model: "qwen/qwen-2.5-coder-32b-instruct",
+		Messages: []Message{
+			{
+				Role:    "user",
+				Content: "What is the meaning of life?",
+			},
+		},
+	}
+
+	// convert request body to json
+	requestBodyJson, err := json.Marshal(requestBody)
+	if err != nil {
+		panic(err)
+	}
+
+	// create request
+	req, err := http.NewRequest("POST", config.ApiBaseUrl, bytes.NewBuffer(requestBodyJson))
+	if err != nil {
+		panic(err)
+	}
+
+	// set headers
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", config.ApiKey))
+
+	// send request
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+
+	// check if status is 200
+	if resp.StatusCode != 200 {
+		panic("status is not 200")
+	}
+
+	defer resp.Body.Close()
+
+	responseBody, err := io.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	var apiResponseBody ApiResponseBody
+	if err := json.Unmarshal(responseBody, &apiResponseBody); err != nil {
+		panic(err)
+	}
+
+	return apiResponseBody.Choices[0].Message.Content
+}
 
 func main() {
-	fmt.Println("Hello World")
+	config, err := LoadConfig("config.toml")
+	if err != nil {
+		panic(err)
+	}
+
+	response := llm(*config)
+	fmt.Println(response)
 }
